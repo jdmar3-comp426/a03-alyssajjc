21705fc00dfad0fb729d8e1be41c84b1e6d7fe63
alyssajjc <junwei@live.unc.edu>
2021-11-16 11:24:46 -0500

    tests

--- src/nodetest.js
+++ src/nodetest.js
@@ -1,6 +1,8 @@
 import { sumToString, getIncreasingArray, maxAndMin, countArray } from "./mild/mild_1.js";
 import { identifyVariable, identifyArray, removeKey, removeKeyNonDestructive, removeKeys } from "./mild/mild_2.js";
 import { getSum, getMedian, getStatistics } from "./medium/medium_1.js";
+import { allCarStats } from "./medium/medium_2.js";
+import mpg_data from "./medium/data/mpg_data.js";
 
 // mild 1 tests
 /* console.log(sumToString(1,2));
@@ -23,10 +25,17 @@ removeKey(obj, 'password');
 console.log(obj); */
 
 // medium 1 tests
-console.log(getSum([1,2,3])); // 6
+/* console.log(getSum([1,2,3])); // 6
 let array = [7,4,2,7,3,2,5,6,2,5]; 
 console.log(array.sort(function(a,b) { return a-b }));
 console.log(getMedian(array)); // 4.5
 console.log(getMedian([7, 2, 3, 5 , 1])); // 3
 console.log(getMedian([7, 7, 3, 2, 8, 5, 4, 18, 1])); // 5
-//console.log(getStatistics([3,2,4,5,5,5,2,6,7]));
+//console.log(getStatistics([3,2,4,5,5,5,2,6,7])); */
+
+// medium 2 tests
+console.log(allCarStats);
+/* console.log(mpg_data.length);
+console.log(mpg_data.reduce((previous, current) => previous + current['city_mpg'], 0) / mpg_data.length);
+console.log(getStatistics(mpg_data.map(function(item) { return item['year']})));
+console.log(mpg_data.filter(current => current.hybrid == true).length); */
