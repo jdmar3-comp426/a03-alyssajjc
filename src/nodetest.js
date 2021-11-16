a073d1b757c68a8045d918e7626d4994abd89a2e
alyssajjc <junwei@live.unc.edu>
2021-11-15 23:39:24 -0500

    identifyVariable test

--- src/nodetest.js
+++ src/nodetest.js
@@ -1,5 +1,9 @@
 import { sumToString, getIncreasingArray, maxAndMin, countArray } from "./mild/mild_1.js";
+import { identifyVariable } from "./mild/mild_2.js";
+
 console.log(sumToString(1,2));
 console.log(getIncreasingArray(5, 8));
 console.log(maxAndMin([3,7,8,2]));
 console.log(countArray([3,3,5,6,5, 'a', 'a']));
+console.log(identifyVariable("13"));
+console.log(identifyVariable(8));
