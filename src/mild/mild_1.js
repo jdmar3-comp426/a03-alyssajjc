2bd50edda3292135d69667ea28e078e7861c0bc9
alyssajjc <junwei@live.unc.edu>
2021-11-15 23:24:16 -0500

    max and min test 3?

--- src/mild/mild_1.js
+++ src/mild/mild_1.js
@@ -40,8 +40,9 @@ export function getIncreasingArray(startNumber, endNumber) {
  * and https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
  */
 export function maxAndMin(numbers) {
-    let min = Math.min(numbers);
-    let max = Math.max(numbers);
+    console.log(numbers.toString)
+    let min = Math.min(...numbers);
+    let max = Math.max(...numbers);
     return `{ max: ${max}, min: ${min}}`
 }
 
--- src/nodetest.js
+++ src/nodetest.js
@@ -1,3 +1,4 @@
-import { sumToString, getIncreasingArray } from "./mild/mild_1.js";
+import { sumToString, getIncreasingArray, maxAndMin } from "./mild/mild_1.js";
 console.log(sumToString(1,2));
 console.log(getIncreasingArray(5, 8));
+console.log(maxAndMin([3,7,8,2]));
