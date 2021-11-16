7463121b3f99f9fc59480bd5f6b3c0acc0ebaf3b
alyssajjc <junwei@live.unc.edu>
2021-11-16 00:02:23 -0500

    remove keys tests

--- src/mild/mild_2.js
+++ src/mild/mild_2.js
@@ -95,5 +95,9 @@ export function removeKeyNonDestructive(object, key) {
  * @return {*} The object with its keys removed.
  */
 export function removeKeys(object, keyList) {
-
+   for (let i = 0; i < keyList.length; i++) {
+      key = keyList[i];
+      current = removeKeyNonDestructive(object, key);
+   }
+   return current;
 }
